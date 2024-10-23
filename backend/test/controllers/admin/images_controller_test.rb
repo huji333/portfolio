10f5295fd66fddac714244fc8f5cbdba23f0e2e6
huji333 <hu203330@gmail.com>
2024-10-23 21:05:43 +0900

    add test

--- backend/test/controllers/admin/images_controller_test.rb
+++ backend/test/controllers/admin/images_controller_test.rb
@@ -1,7 +1,29 @@
 require "test_helper"
 
 class Admin::ImagesControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  include Warden::Test::Helpers
+
+  setup do
+    @admin = users(:one)
+    @guest = users(:two)
+  end
+
+  test 'admin should get index' do
+    login_as(@admin)
+    get admin_images_path
+    assert_response :success
+  end
+
+  test 'guest should not get index' do
+    login_as(@guest)
+    get admin_images_path
+    response = JSON.parse(@response.body)
+    assert_equal 'You are not authorized to perform this action.', response['error']
+  end
+
+  test 'logged out user should not get index' do
+    get admin_images_path
+     assert_response :redirect
+    assert_redirected_to new_user_session_path
+  end
 end
