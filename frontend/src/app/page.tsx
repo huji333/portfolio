a6c486c9b8fc26d47963ae23d3b0b1e4622bdca3
huji333 <hu203330@gmail.com>
2024-11-30 22:23:47 +0900

    cseate page

---
+++ frontend/src/app/page.tsx
@@ -0,0 +1,20 @@
+import Image from "next/image";
+import pcImage from "../public/index-pc.jpg";
+import phImage from "../public/index-ph.jpg";
+
+export default function Home() {
+  return (
+    <div className="relative h-screen bg-cover bg-center bg-[url('/index-ph.jpg')] md:bg-[url('/index-pc.jpg')]">
+      <div className="absolute top-1/4 left-1/2 transform -translate-x-1/2 md:hidden">
+        <h1 className="text-6xl font-bold text-black">Kakemu FUJII</h1>
+      </div>
+      <div className="hidden md:flex absolute inset-0 items-center justify-center">
+        <h1 className="text-6xl font-bold text-white drop-shadow-lg">
+          Kakemu FUJII
+        </h1>
+      </div>
+    </div>
+
+
+  );
+}
