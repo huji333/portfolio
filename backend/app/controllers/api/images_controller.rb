7669a0b4c129b42ad1537e9dcbbb5f36caa1c51c
huji333 <hu203330@gmail.com>
2025-04-16 16:46:38 +0900

    fix index

--- backend/app/controllers/api/images_controller.rb
+++ backend/app/controllers/api/images_controller.rb
@@ -1,13 +1,15 @@
 class Api::ImagesController < ApplicationController
+  include Rails.application.routes.url_helpers
+
   def index
-    @images = Image.all
-    render json: @images
-  end
-  def show
-    @image = Image.find(params[:id])
-    render json: {
-      image: @image,
-      file: @image.file.url
+    images = Image.with_attached_file.order(:display_order)
+    render json: images.map { |image|
+      image.as_json.merge(file: url_for(image.file))
     }
+  end
+
+  def show
+    image = Image.find(params[:id])
+    render json: image.as_json.merge(file: url_for(image.file))
   end
 end
