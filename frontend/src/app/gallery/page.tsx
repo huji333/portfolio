e9788db587664dcf825b3bfe2a9df8c5f4028cf0
huji333 <hu203330@gmail.com>
2025-04-19 19:09:03 +0900

    fix header

--- frontend/src/app/gallery/page.tsx
+++ frontend/src/app/gallery/page.tsx
@@ -2,16 +2,11 @@ import React, { Suspense } from "react";
 import Header from "../components/layout/header";
 import GalleryApp from '../containers/galleryapp';
 
-
-
 export default function Page() {
   return (
-
     <>
-      <div className="w-full h-auto bg-[#faf7f2]">
-        <Header />
-      </div>
-      <div className="min-h-screen bg-[#faf7f2]">
+      <Header />
+      <div className="min-h-screen bg-[#faf7f2] pt-[80px]">
         <Suspense>
           <GalleryApp />
         </Suspense>
