a1174b84a8862fec06ba780be6133b1c9b2682ae
huji333 <hu203330@gmail.com>
2025-05-14 21:35:45 +0900

    use useeffect

--- frontend/src/app/containers/galleryapp.tsx
+++ frontend/src/app/containers/galleryapp.tsx
@@ -1,12 +1,27 @@
+"use client"
 import { ImageType } from '@/utils/types';
-import { use } from 'react';
+import { CategoryType } from '@/utils/types';
+import { use, useState, useEffect } from 'react';
 import ImageGrid from '../components/image/imagegrid';
+import CategoryCheckboxList from '../components/image/categorycheckboxlist';
 
 type Props = {
   categories: number[];
 };
 
-async function getImages(categories : number[]): Promise<ImageType[]> {
+
+
+async function fetchCategories(){
+  const url = `${process.env.NEXT_PUBLIC_API_BASE_URL}/categories`;
+  const res = await fetch(url);
+  if (!res.ok){
+    throw new Error("Failed to fetch categories.");
+  }
+  const categoriesData: CategoryType[] = await res.json()
+  return categoriesData
+}
+
+async function fetchImages(categories : number[]): Promise<ImageType[]> {
   const query = categories.length > 0 ? `?categories=${categories.join(',')}` : '';
   const url = `${process.env.NEXT_PUBLIC_API_BASE_URL}/images${query}`;
   console.log(url);
@@ -18,8 +33,24 @@ async function getImages(categories : number[]): Promise<ImageType[]> {
   return ImagesData;
 }
 
-export default function GalleryApp({categories}: Props) {
-  const images = use(getImages(categories))
+
+export default function Gallerypp() {
+  const [selectedCategories, setSelectedCategories] = useState<number[]>([]);
+  const [images, setImages] = useState<ImageType[]>([])
+  const [categories, setCategories] = useState<number[]>([])
+
+  console.log("page")
+
+  async function updateImages(categoryids: number[]){
+    const images = await fetchImages(categoryids)
+    setImages(images)
+  }
+
+  useEffect( () => {
+    updateImages(categories)
+  },[]
+  )
+
   return (
     <ImageGrid images={images} />
   );
