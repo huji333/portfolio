65baeefd2ea022bddb2645aa718b102b66232a80
huji333 <hu203330@gmail.com>
2025-05-15 23:50:56 +0900

    fix cors

---
+++ backend/config/initializers/cors.rb
@@ -0,0 +1,9 @@
+Rails.application.config.middleware.insert_before 0, Rack::Cors do
+  allow do
+    origins ENV['FRONTEND_ORIGIN'] || "http://localhost:3001"
+    resource "*",
+             headers: :any,
+             methods: %i[get post put patch delete options head],
+             expose: %w[access-token expiry token-type uid client]
+  end
+end
