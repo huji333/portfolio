b28f089098f0233674ada286643cfad0758c83d6
huji333 <hu203330@gmail.com>
2025-05-24 22:30:12 +0900

    fix rubo

--- backend/config/application.rb
+++ backend/config/application.rb
@@ -15,7 +15,7 @@ module Backend
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
     # Common ones are `templates`, `generators`, or `middleware`, for example.
     config.autoload_paths << Rails.root.join('lib')
-    config.autoload_paths << Rails.root.join('app', 'models', 'concerns')
+    config.autoload_paths << Rails.root.join("app/models/concerns")
 
     # Configuration for the application, engines, and railties goes here.
     #
